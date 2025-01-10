532b1745280180a509f20e4578ce46dd48a0b150
Andrey <aprishlov@gmail.com>
2025-01-10 15:57:08 +0300

    feat: created PlayerResponceDTO

---
+++ src/main/java/prog/rohan/tennis_scoreboard/dto/PlayerResponseDTO.java
@@ -0,0 +1,13 @@
+package prog.rohan.tennis_scoreboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PlayerResponseDTO {
+    Long id;
+    String name;
+}
