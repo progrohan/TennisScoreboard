09c289d93683aa2b4848d7df3d7b16a41c4b9f07
Andrey <aprishlov@gmail.com>
2025-01-10 16:55:32 +0300

    feat: created InvalidDataException

---
+++ src/main/java/prog/rohan/tennis_scoreboard/exceptions/InvalidDataException.java
@@ -0,0 +1,7 @@
+package prog.rohan.tennis_scoreboard.exceptions;
+
+public class InvalidDataException extends RuntimeException{
+    public InvalidDataException(String msg){
+        super(msg);
+    }
+}
