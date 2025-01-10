79deb238d599b2d1d86ad3cd87dec33c87eb3098
Andrey <aprishlov@gmail.com>
2025-01-10 15:56:42 +0300

    feat: created OngoingMatchDTO

---
+++ src/main/java/prog/rohan/tennis_scoreboard/dto/OngoingMatchDTO.java
@@ -0,0 +1,28 @@
+package prog.rohan.tennis_scoreboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class OngoingMatchDTO {
+    private UUID uuid;
+
+    private PlayerResponseDTO firstPlayer;
+    private PlayerResponseDTO secondPlayer;
+
+    private int firstPlayerSets;
+    private int secondPlayerSets;
+
+    private int firstPlayerGames;
+    private int secondPlayerGames;
+
+    private int firstPlayerPoints;
+    private int secondPlayerPoints;
+}
