f528da64ceaf9bfc8c068e50639f8529b2719f79
Andrey <aprishlov@gmail.com>
2025-01-15 10:31:51 +0300

    feat: creates DataBaseException

---
+++ src/main/java/prog/rohan/tennis_scoreboard/exceptions/DataBaseException.java
@@ -0,0 +1,7 @@
+package prog.rohan.tennis_scoreboard.exceptions;
+
+public class DataBaseException extends RuntimeException{
+    public DataBaseException(String message){
+        super(message);
+    }
+}
