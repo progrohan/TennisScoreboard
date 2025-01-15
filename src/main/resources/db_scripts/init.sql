2b9a1118195b5977a011f648fe6c76e0ebee3bf0
Andrey <aprishlov@gmail.com>
2025-01-15 10:30:24 +0300

    chore: chanded init db scripts

--- src/main/resources/db_scripts/init.sql
+++ src/main/resources/db_scripts/init.sql
@@ -1,32 +1,15 @@
+INSERT INTO Players (id, name) VALUES (default, 'E. Ivanov');
+INSERT INTO Players (id, name) VALUES (default, 'K. Petrova');
+INSERT INTO Players (id, name) VALUES (default, 'M. Sidorov');
+INSERT INTO Players (id, name) VALUES (default, 'L. Kravtsov');
+INSERT INTO Players (id, name) VALUES (default, 'P. Smirnova');
 
-CREATE TABLE Players (
-                         ID INT AUTO_INCREMENT PRIMARY KEY,
-                         Name VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Matches (
-                         ID INT AUTO_INCREMENT PRIMARY KEY,
-                         Player1 INT NOT NULL,
-                         Player2 INT NOT NULL,
-                         Winner INT NOT NULL,
-                         FOREIGN KEY (Player1) REFERENCES Players(ID),
-                         FOREIGN KEY (Player2) REFERENCES Players(ID),
-                         FOREIGN KEY (Winner) REFERENCES Players(ID)
-);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 1, 2, 2);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 3, 4, 3);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 5, 1, 5);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 2, 4, 4);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 3, 5, 3);
 
 
-INSERT INTO Players (Name) VALUES
-                               ('Alice'),
-                               ('Bob'),
-                               ('Charlie'),
-                               ('Diana');
-
-
-INSERT INTO Matches (Player1, Player2, Winner) VALUES
-                                                   (1, 2, 1), -- Матч между Alice и Bob, победила Alice
-                                                   (1, 3, 3), -- Матч между Alice и Charlie, победил Charlie
-                                                   (2, 3, 2), -- Матч между Bob и Charlie, победил Bob
-                                                   (2, 4, 4), -- Матч между Bob и Diana, победила Diana
-                                                   (3, 4, 3); -- Матч между Charlie и Diana, победил Charlie
 
 
