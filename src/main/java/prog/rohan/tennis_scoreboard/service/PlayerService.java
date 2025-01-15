e40f623f59fa425f3baab9884f04bc6e0cfa2f6f
Andrey <aprishlov@gmail.com>
2025-01-15 10:34:45 +0300

    feat: PlayerService is done

--- src/main/java/prog/rohan/tennis_scoreboard/service/PlayerService.java
+++ src/main/java/prog/rohan/tennis_scoreboard/service/PlayerService.java
@@ -1,16 +1,22 @@
 package prog.rohan.tennis_scoreboard.service;
 
 import prog.rohan.tennis_scoreboard.dto.PlayerDTO;
+import prog.rohan.tennis_scoreboard.entity.Player;
+import prog.rohan.tennis_scoreboard.repository.PlayerRepositrory;
+
+import java.util.Optional;
 
 public class PlayerService {
-    //PlayerDAO playerDAO = PlayerDAO.getInstance();
+    PlayerRepositrory playerRepositrory = PlayerRepositrory.getINSTANCE();
 
-    public PlayerDTO saveIfNotExists(String name){
-        PlayerDTO playerResponseDTO = new PlayerDTO(0L,name);
-        // Player player = PlayerDAO.saveIfNotExists(name)
-        //playerResponseDTO = map Player -> PlayerResponseDTO
+    public PlayerDTO save(String name){
+        Optional<Player> optionalPlayer = playerRepositrory.findByName(name);
 
-        return playerResponseDTO;
+        Player player = optionalPlayer
+                .orElseGet(() -> playerRepositrory
+                        .save(new Player(null, name)));
+
+        return new PlayerDTO(player.getId(), player.getName());
     }
 
 }
