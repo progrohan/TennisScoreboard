d356a3717c9ca44ee37b678e823da0e1edb21f61
Andrey <aprishlov@gmail.com>
2025-01-17 19:54:15 +0300

    feat: created MatchScoreServlet

---
+++ src/main/java/prog/rohan/tennis_scoreboard/servlets/MatchScoreServlet.java
@@ -0,0 +1,60 @@
+package prog.rohan.tennis_scoreboard.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import prog.rohan.tennis_scoreboard.dto.FinishedMatchDTO;
+import prog.rohan.tennis_scoreboard.dto.OngoingMatchDTO;
+import prog.rohan.tennis_scoreboard.service.FinishedMatchesPersistenceService;
+import prog.rohan.tennis_scoreboard.service.MatchScoreCalculationService;
+import prog.rohan.tennis_scoreboard.service.OngoingMatchService;
+import prog.rohan.tennis_scoreboard.service.PlayerService;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet(name = "match-score", value = "/match-score")
+public class MatchScoreServlet extends HttpServlet {
+    private final OngoingMatchService ongoingMatchService = OngoingMatchService.getINSTANCE();
+    private final MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+    private final FinishedMatchesPersistenceService finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
+    private final PlayerService playerService = new PlayerService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+
+        OngoingMatchDTO ongoingMatch = ongoingMatchService.getByUUId(uuid);
+
+        req.setAttribute("ongoingMatch", ongoingMatch);
+        req.getRequestDispatcher("/jsp/match-score.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        Long winnerId = Long.valueOf(req.getParameter("playerId"));
+
+        OngoingMatchDTO ongoingMatch = ongoingMatchService.getByUUId(uuid);
+
+        ongoingMatch = matchScoreCalculationService.increaseScore(ongoingMatch, winnerId);
+
+        if(matchScoreCalculationService.isMatchOver(ongoingMatch)){
+            FinishedMatchDTO finishedMatch = FinishedMatchDTO.builder()
+                    .firstPlayer(ongoingMatch.getFirstPlayer())
+                    .secondPlayer(ongoingMatch.getSecondPlayer())
+                    .winner(playerService.findById(ongoingMatch.getWinnerId()))
+                    .build();
+            finishedMatchesPersistenceService.save(finishedMatch);
+            req.setAttribute("match", ongoingMatch);
+            ongoingMatchService.deleteMatch(uuid);
+            req.getRequestDispatcher("/jsp/final-score.jsp").forward(req, resp);
+        }else{
+            req.setAttribute("ongoingMatch", ongoingMatch);
+            req.getRequestDispatcher("/jsp/match-score.jsp").forward(req, resp);
+        }
+    }
+}
