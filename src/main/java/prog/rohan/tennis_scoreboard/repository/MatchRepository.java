8ab527cf4ef1942a91a3772b050c2c6a5c6a9c4b
Andrey <aprishlov@gmail.com>
2025-01-19 01:52:32 +0300

    feat: created findWithPagination method

--- src/main/java/prog/rohan/tennis_scoreboard/repository/MatchRepository.java
+++ src/main/java/prog/rohan/tennis_scoreboard/repository/MatchRepository.java
@@ -1,7 +1,12 @@
 package prog.rohan.tennis_scoreboard.repository;
 
+import jakarta.persistence.criteria.CriteriaQuery;
 import lombok.Getter;
+import org.hibernate.Session;
 import prog.rohan.tennis_scoreboard.entity.Match;
+import prog.rohan.tennis_scoreboard.exceptions.DataBaseException;
+
+import java.util.List;
 
 public class MatchRepository extends BaseRepository<Long, Match>{
     @Getter
@@ -9,6 +14,18 @@ public class MatchRepository extends BaseRepository<Long, Match>{
 
     private MatchRepository() {
         super(Match.class);
+    }
+
+    public List<Match> findWithPagination(int offset, int count){
+        try(Session session = sessionFactory.openSession()){
+            List<Match> result = session.createQuery("FROM Match ORDER BY id DESC", Match.class)
+                    .setFirstResult(offset * count + 1)
+                    .setMaxResults(count)
+                    .getResultList();
+            return result;
+        }catch(Exception e){
+            throw new DataBaseException("Problem with finding with pagination in database!");
+        }
     }
 
 }
