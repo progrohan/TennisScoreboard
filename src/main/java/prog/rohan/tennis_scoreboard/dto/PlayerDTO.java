6c6e1113edd03195025d295b6b19689eca775125
Andrey <aprishlov@gmail.com>
2025-01-19 01:49:29 +0300

    chore: added NoArgsConstructor

--- src/main/java/prog/rohan/tennis_scoreboard/dto/PlayerDTO.java
+++ src/main/java/prog/rohan/tennis_scoreboard/dto/PlayerDTO.java
@@ -2,11 +2,13 @@ package prog.rohan.tennis_scoreboard.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PlayerDTO {
     Long id;
     String name;
