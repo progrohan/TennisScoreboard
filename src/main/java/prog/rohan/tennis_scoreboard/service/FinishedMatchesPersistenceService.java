af99da36095915837143566caf3e8f61422b97c8
Andrey <aprishlov@gmail.com>
2025-01-19 01:54:03 +0300

    feat: created get, findAll and findWithPagination methods

--- src/main/java/prog/rohan/tennis_scoreboard/service/FinishedMatchesPersistenceService.java
+++ src/main/java/prog/rohan/tennis_scoreboard/service/FinishedMatchesPersistenceService.java
@@ -2,24 +2,38 @@ package prog.rohan.tennis_scoreboard.service;
 
 import prog.rohan.tennis_scoreboard.dto.FinishedMatchDTO;
 import prog.rohan.tennis_scoreboard.entity.Match;
-import prog.rohan.tennis_scoreboard.entity.Player;
+import prog.rohan.tennis_scoreboard.mapper.MatchMapper;
 import prog.rohan.tennis_scoreboard.repository.MatchRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class FinishedMatchesPersistenceService {
     MatchRepository matchRepository = MatchRepository.getINSTANCE();
 
     public void save(FinishedMatchDTO matchDTO){
-        Match match = Match.builder()
-                .player1(new Player(matchDTO.getFirstPlayer().getId(),
-                        matchDTO.getFirstPlayer().getName()))
-                .player2(new Player(matchDTO.getSecondPlayer().getId(),
-                        matchDTO.getSecondPlayer().getName()))
-                .winner(new Player(matchDTO.getWinner().getId(),
-                        matchDTO.getWinner().getName()))
-                .build();
+        Match match = MatchMapper.INSTANCE.toEntity(matchDTO);
 
        matchRepository.save(match);
+    }
+
+    public int count(){
+        return matchRepository.findAll().size();
+    }
+
+    public List<FinishedMatchDTO> findAll(){
+        List<Match> matches = matchRepository.findAll();
+        return matches
+                .stream()
+                .map(MatchMapper.INSTANCE::toDto).collect(Collectors.toList());
+    }
+
+    public List<FinishedMatchDTO> findWithPagination(int offset, int count){
+        List<Match> matches = matchRepository.findWithPagination(offset, count);
+        return matches
+                .stream()
+                .map(MatchMapper.INSTANCE::toDto).collect(Collectors.toList());
     }
 
 }
