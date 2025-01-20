8165a2e63e8061dd3a447c9708a09e2cd498c025
Andrey <aprishlov@gmail.com>
2025-01-20 14:05:22 +0300

    feat: created findAllWithFilter and findWithFilterAndPagination methods

--- src/main/java/prog/rohan/tennis_scoreboard/repository/MatchRepository.java
+++ src/main/java/prog/rohan/tennis_scoreboard/repository/MatchRepository.java
@@ -19,12 +19,39 @@ public class MatchRepository extends BaseRepository<Long, Match>{
     public List<Match> findWithPagination(int offset, int count){
         try(Session session = sessionFactory.openSession()){
             List<Match> result = session.createQuery("FROM Match ORDER BY id DESC", Match.class)
-                    .setFirstResult(offset * count + 1)
+                    .setFirstResult(offset)
                     .setMaxResults(count)
                     .getResultList();
             return result;
         }catch(Exception e){
             throw new DataBaseException("Problem with finding with pagination in database!");
+        }
+    }
+
+    public List<Match> findAllWithFilter(String filter){
+        try(Session session = sessionFactory.openSession()){
+            List<Match> result = session
+                    .createQuery("FROM Match WHERE LOWER(player1.name) LIKE :filter or LOWER(player2.name) LIKE :filter ORDER BY id DESC", Match.class)
+                    .setParameter("filter", filter)
+                    .getResultList();
+            String pom = "bom";
+            return result;
+        }catch(Exception e){
+            throw new DataBaseException("Problem with finding all with filter in database!");
+        }
+    }
+
+    public List<Match> findWithFilterAndPagination(String filter,int offset, int count){
+        try(Session session = sessionFactory.openSession()){
+            List<Match> result = session
+                    .createQuery("FROM Match WHERE player1.name = :filter or player2.name = :filter ORDER BY id DESC", Match.class)
+                    .setParameter("filter", filter)
+                    .setFirstResult(offset)
+                    .setMaxResults(count)
+                    .getResultList();
+            return result;
+        }catch(Exception e){
+            throw new DataBaseException("Problem with finding with filter and pagination in database!");
         }
     }
 
