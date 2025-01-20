655af1819972448110197d6baefdcf25a8c14bd6
Andrey <aprishlov@gmail.com>
2025-01-20 14:01:48 +0300

    chore: added more init scripts

--- src/main/resources/db_scripts/init.sql
+++ src/main/resources/db_scripts/init.sql
@@ -3,12 +3,38 @@ INSERT INTO Players (id, name) VALUES (default, 'K. Petrova');
 INSERT INTO Players (id, name) VALUES (default, 'M. Sidorov');
 INSERT INTO Players (id, name) VALUES (default, 'L. Kravtsov');
 INSERT INTO Players (id, name) VALUES (default, 'P. Smirnova');
+INSERT INTO Players (id, name) VALUES (default, 'A. Ivanov');
+INSERT INTO Players (id, name) VALUES (default, 'E. Petrova');
+INSERT INTO Players (id, name) VALUES (default, 'K. Voronov');
+INSERT INTO Players (id, name) VALUES (default, 'L. Kuznetsova');
+INSERT INTO Players (id, name) VALUES (default, 'D. Pavlov');
+INSERT INTO Players (id, name) VALUES (default, 'N. Frolova');
+INSERT INTO Players (id, name) VALUES (default, 'S. Mironov');
+INSERT INTO Players (id, name) VALUES (default, 'O. Solovyeva');
+INSERT INTO Players (id, name) VALUES (default, 'I. Karpov');
+INSERT INTO Players (id, name) VALUES (default, 'T. Guseva');
+INSERT INTO Players (id, name) VALUES (default, 'V. Smolenskaya');
+INSERT INTO Players (id, name) VALUES (default, 'P. Mikhailov');
 
 INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 1, 2, 2);
 INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 3, 4, 3);
 INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 5, 1, 5);
 INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 2, 4, 4);
 INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 3, 5, 3);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 1, 2, 2);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 3, 4, 4);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 5, 6, 5);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 7, 8, 8);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 9, 10, 10);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 11, 12, 11);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 13, 14, 13);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 15, 16, 16);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 1, 3, 1);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 2, 4, 2);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 5, 7, 5);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 6, 8, 6);
+INSERT INTO Matches (id, player1, player2, winner) VALUES (default, 9, 11, 11);
+
 
 
 
