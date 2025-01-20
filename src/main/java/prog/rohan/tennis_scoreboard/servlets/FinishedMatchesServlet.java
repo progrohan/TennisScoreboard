88673e7a539efab68d01ace5f9d1b6100637d863
Andrey <aprishlov@gmail.com>
2025-01-20 14:10:41 +0300

    feat: added pagination

--- src/main/java/prog/rohan/tennis_scoreboard/servlets/FinishedMatchesServlet.java
+++ src/main/java/prog/rohan/tennis_scoreboard/servlets/FinishedMatchesServlet.java
@@ -10,6 +10,7 @@ import prog.rohan.tennis_scoreboard.service.FinishedMatchesPersistenceService;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @WebServlet(name="matches", value = "/matches")
 public class FinishedMatchesServlet extends HttpServlet {
@@ -19,11 +20,26 @@ public class FinishedMatchesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int page = Integer.parseInt(req.getParameter("page"));
+        if (page == 0) page = 1;
+
         String filterByName = req.getParameter("filter_by_player_name");
 
-        int matchesCount = matchesService.count();
-        int pagesCount = matchesCount / MATCHES_ON_PAGE;
-        List<FinishedMatchDTO> matches = matchesService.findWithPagination(page * MATCHES_ON_PAGE + 1, MATCHES_ON_PAGE);
+        List<FinishedMatchDTO> matches;
+        int matchesCount;
+        int pagesCount;
+
+        if(Objects.equals(filterByName, "")){
+            matchesCount = matchesService.count();
+            matches = matchesService.findWithPagination((page - 1) * MATCHES_ON_PAGE, MATCHES_ON_PAGE);
+        }else{
+            matchesCount = matchesService.countWithFilter(filterByName);
+            matches = matchesService.findWithFilterAndPagination(filterByName,(page - 1) * MATCHES_ON_PAGE, MATCHES_ON_PAGE);
+        }
+        pagesCount =(matchesCount + MATCHES_ON_PAGE - 1) / MATCHES_ON_PAGE;
+
+
+        req.setAttribute("currentPage", page);
+        req.setAttribute("totalPages", pagesCount);
         req.setAttribute("matches", matches);
         req.getRequestDispatcher("/jsp/matches.jsp").forward(req, resp);
     }
